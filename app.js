20db5c0c829f1c8efb219add5cb882bb6259a744
Borja Gete <borjagete90@outlook.es>
2018-11-16 08:53:32 +0000

    Seeding Database

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds");
     
+seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//SCHEMA SETUP
-var campgroundSchema = new mongoose.Schema({
-    name:  String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create({
-//     name: "Salmon Creek",
-//     image: "https://images.unsplash.com/photo-1496425745709-5f9297566b46?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b084690f83c5e63fafd161f8bc729a1f&auto=format&fit=crop&w=500&q=60",
-//     description: "This is a huge salmon river, no bathrooms."
-// }, function(err, campground) {
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log("NEWLY CREATED CAMPGROUND: ");
-//         console.log(campground);
-//     }
-// });
 
 app.get("/",function(req, res){
     res.render("landing")
@@ -69,11 +51,11 @@ app.get("/campgrounds/new",function(req, res) {
 
 //SHOW
 app.get("/campgrounds/:id", function(req, res) {
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
             console.log(err);
         } else {
-            res.render("show", {campground: foundCampground})
+            res.render("show", {campground: foundCampground});
         }
     });
 })
