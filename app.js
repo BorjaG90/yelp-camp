fcae87066a59f5e695e7dc4cdbd5d246b2e72a27
Borja Gete <borjagete90@outlook.es>
2018-11-21 09:03:08 +0000

    YelpCamp: User Associatons: Comment

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes          = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/yelp_camp_v8");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+
+//seedDB(); // seed the database
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
