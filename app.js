ca58320033f9740c98142669ac1e8925652acbba
Borja Gete <borjagete90@outlook.es>
2018-11-26 09:01:17 +0000

    Add Dynamic Price Feature

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect("mongodb://localhost/yelp_camp_v13");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,12 +22,13 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     //get data from form and add to campground array
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name, price: price, image: image, description: desc, author: author};
 
     //Create and save to DB the new
     Campground.create(newCampground,function(err, newlyCreated) {
